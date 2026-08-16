8d697be57c41c02c0f98b8184bc93dcac55cc1c5
Olexandr <sashamischishin2005@gmail.com>
2026-08-16 20:24:14 +0300

    Fix production asset paths

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { resolve } from "node:path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "./",
+  base: "/",
   build: {
     rollupOptions: {
       input: {
